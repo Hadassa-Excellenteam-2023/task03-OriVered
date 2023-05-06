1cd43cd60c348ddfd3978a5f1a04988653c88a31
OriVered <ori.vered@gmail.com>
2023-05-06 17:38:09 +0300

    Submit

--- include/PriorityQueue.h
+++ include/PriorityQueue.h
@@ -4,16 +4,74 @@
 #include <list>
 #include <exception>
 
+/* Struct Comparator Require for Priority Queue*/
+template<typename T>
+struct MyComparator {
+	T operator()(T a, T b) const {
+		return a - b;
+	}
+};
 
-// any templates?
+/* Requirements for template */
+template <typename T>
+concept Streamable = requires(T a, std::ostream & os) {
+	{ os << a };
+};
+
+template <typename T>
+concept Subtractable = requires(T a, T b) {
+	{ a - b } -> std::same_as<T>;
+};
+
+template <typename T>
+concept ComparableToInteger = requires(T a, int i) {
+	{ a < i } -> std::convertible_to<bool>;
+};
+
+/* PriorityQueue class template*/
+template <typename T>
 class PriorityQueue {
-   
+	using list = std::list<T>;
+	using exception = std::exception;
 public:
-	
-   // You need to complete the implement : 
+
+	/* Constructor */
+	PriorityQueue();
+
+	/* Mutator */
 	void push(const T& t); 
+
+	/* Accessor */
 	T poll();
 
 private:
-// add relevant data members
+	/* Privete member*/
+	list m_queue;
 };
+
+template<typename T>
+PriorityQueue<T>::PriorityQueue()
+	: m_queue(){
+}
+
+template<typename T>
+void PriorityQueue<T>::push(const T& t) {
+    auto it = m_queue.begin();
+    while (it != m_queue.end() && MyComparator<T>{}(*it, t) < 0) {
+        ++it;
+    }
+    m_queue.insert(it, t);
+}
+
+template<typename T>
+T PriorityQueue<T>::poll() {
+	if (m_queue.empty()) {
+		throw std::exception("PriorityQueue empty!");
+	}
+
+	T highest_priority_element = m_queue.front();
+	m_queue.erase(m_queue.begin());
+	return highest_priority_element;
+}
+
+
